ac9ca3799fe2a745e2cb1ac765e83dfe0f60c3c2
houjiabing <3109812974@qq.com>
2019-01-17 18:02:44 +0800

    Create connect_mysql_get_data.sh
    从数据库拉取数据，并且循环执行脚本

---
+++ connect_mysql_get_data.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+MYSQL_HOST='*.*.*.*.*'
+MYSQL_USER='****'
+MYSQL_PASSWD='*****'
+MYSQL_DB='*****'
+MYSQL_PORT='*****'
+COMMAND="mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWD} ${MYSQL_DB}"
+
+
+//注释里的是可以手动执行的，但是，在cron 是报错的
+/*
+while read -a row
+do
+        /home/oicq/wb/sync_data_svr/tools/hash_data_cache_tool 0 ${row[0]} ${row[1]}
+done< <(echo "select proc_id,unit_id from wb_note;" | ${COMMAND}) | awk 'NR>1'
+*/
+//解决办法：把数据输出到文件，然后执行
+DATUM= ${COMMAND} -e "select proc_id,unit_id from wb_note;" | awk 'NR>1' >/home/oicq/wb/un_pr.log
+while read -a row
+do
+//执行脚本
+/home/oicq/wb/sync_data_svr/tools/hash_data_cache_tool 2 ${row[0]} ${row[1]}
+
+/home/oicq/wb/sync_data_svr/tools/modify_status 0 ${row[1]} 2
+
+done < /home/oicq/wb/un_pr.log
+
+
